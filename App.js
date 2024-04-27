0b6646af48958a529810ce17e809518b0d1fd6c2
Van Dat <ddat888@gmail.com>
2024-04-27 20:30:20 +0700

    feat: avatar(error), getUser, singleton

--- App.js
+++ App.js
@@ -3,9 +3,13 @@ import { StyleSheet, Text, View, Button } from "react-native";
 import UserService from "./services/user";
 
 export default function App() {
-  const handlePress = () => {
-    const userService = new UserService();
-    userService.createUser("lkdfasjlfka", "Van Dat", "ddat828@gmail.com");
+  const handlePress = async () => {
+    const uid = "lkdfasjlfka";
+    const image = await pickImage();
+    if (image) {
+      UserService.getInstance().setAvatarUrl(uid, image.uri);
+      UserService.getInstance().getAvatarUrl(uid);
+    }
   };
 
   return (
--- services/user.js
+++ services/user.js
@@ -1,4 +1,4 @@
-import { db } from "../firebaseConfig";
+import { db, storage } from "../firebaseConfig";
 import {
   collection,
   getDocs,
@@ -8,17 +8,29 @@ import {
   doc,
   setDoc,
 } from "firebase/firestore";
+import { ref, getStorage, getDownloadURL, put } from "firebase/storage";
 
 const initUser = {
   email: "",
   avatarUrl: "",
   friends: [],
   username: "",
+  uid: "",
 };
 
 class UserService {
   constructor(user = null) {
-    this.user = user || initUser;
+    if (UserService.instace == null) {
+      this.user = user || initUser;
+      UserService.instace = this;
+    }
+    return UserService.instace;
+  }
+  static getInstance() {
+    if (!UserService.instace) {
+      UserService.instace = new UserService();
+    }
+    return UserService.instace;
   }
   async createUser(uid, username, email) {
     console.log("start add user");
@@ -26,6 +38,7 @@ class UserService {
       ...this.user,
       username,
       email,
+      uid,
     };
     try {
       const customRef = uid;
@@ -35,6 +48,31 @@ class UserService {
     } catch (e) {
       console.error("Error adding document: ", e);
     }
+  }
+  async getUser(uid) {
+    console.log(`start get user ${uid}`);
+    const q = query(collection(db, "users"), where("uid", "==", uid));
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      console.log(doc.id, " => ", doc.data());
+    });
+  }
+  async setAvatarUrl(uid, uri) {
+    console.log(`start set avatar url for ${uid}`);
+    const storageRef = ref(storage, `avatars/${uid}`);
+    const response = await fetch(uri);
+    const blob = await response.blob();
+    try {
+      await put(storageRef, blob);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+  async getAvatarUrl(uid) {
+    console.log(`start get avatar url for ${uid}`);
+    const storageRef = ref(storage, `avatars/${uid}`);
+    const url = await getDownloadURL(storageRef);
+    console.log("File available at", url);
   }
 }
 
