5a067a4efdf36e320e777aa5aa955eec5d5fb8fc
Panda Nguyễn <ndbaochau104@gmail.com>
2024-05-03 15:48:50 +0700

    Merge UI/FriendsSpending into UI/Login_SignUp

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import FriendsScreen from "./screens/FriendsScreen";
+import React from 'react';
 import LoginScreen from "./screens/LoginScreen";
 import SignUpScreen from "./screens/SignUpScreen";
 import AppNavigation from "./navigation/appNavigation";
+// import './output.css';
+// import { View, Text } from 'react-native-tailwindcss';
 
+import { tw } from 'tailwind-rn';
 export default function App() {
   return (
+    // <SafeAreaView style={styles.container}>
+    //   <View style={styles.container}>
+    //     <FriendsScreen></FriendsScreen>
+    //   </View>
+    // </SafeAreaView>
     <AppNavigation></AppNavigation>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 100,
+  },
+  innerContainer: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
