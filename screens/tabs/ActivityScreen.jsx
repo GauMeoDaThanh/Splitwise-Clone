2a16448b433c2155134ff121455be6ea37e8b8b8
Tu08112003 <huynhtu012023@gmail.com>
2024-06-05 16:22:20 +0700

    UI: create UI Activity Payment and Expense

--- screens/tabs/ActivityScreen.jsx
+++ screens/tabs/ActivityScreen.jsx
@@ -20,27 +20,29 @@ import ActivityAddMember from "../../components/ActivityAddMember";
 import ActivityEditGroupAvatar from "../../components/ActivityEditGroupAvatar";
 import ActivityService from "../../services/activity";
 import { auth } from "../../firebaseConfig";
+import ActivityExpense from "../../components/ActivityExpense";
+import ActivityPayment from "../../components/ActivityPayment";
 
 const ActivityScreen = () => {
-  const navigation = useNavigation();
-  const [activityData, setActivityData] = useState([]);
-  const [userId, setUserId] = useState(auth.currentUser.uid);
+  // const navigation = useNavigation();
+  // const [activityData, setActivityData] = useState([]);
+  // const [userId, setUserId] = useState(auth.currentUser.uid);
 
-  useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged((user) => {
-      if (user) {
-        setUserId(user.uid);
-      }
-    });
+  // useEffect(() => {
+  //   const unsubscribe = auth.onAuthStateChanged((user) => {
+  //     if (user) {
+  //       setUserId(user.uid);
+  //     }
+  //   });
 
-    return unsubscribe;
-  }, []);
+  //   return unsubscribe;
+  // }, []);
 
-  useEffect(() => {
-    ActivityService.getInstance().listenActivity((data) => {
-      setActivityData(data);
-    });
-  }, [userId]);
+  // useEffect(() => {
+  //   ActivityService.getInstance().listenActivity((data) => {
+  //     setActivityData(data);
+  //   });
+  // }, [userId]);
 
   return (
     <View className="flex-1 bg-white py-5">
@@ -55,7 +57,7 @@ const ActivityScreen = () => {
           Recent activity
         </Text>
       </View>
-      {Array.isArray(activityData) && activityData.every((item) => item) && (
+      {/* {Array.isArray(activityData) && activityData.every((item) => item) && (
         <FlatList
           className="flex-col mb-10"
           showsVerticalScrollIndicator={false}
@@ -64,7 +66,7 @@ const ActivityScreen = () => {
             <NotificationRender type={item.type} data={item} />
           )}
         />
-      )}
+      )} */}
       {/* <ActivityAddGroup></ActivityAddGroup>
         <ActivityDeleteGroup></ActivityDeleteGroup>
         <ActivityEditGroup></ActivityEditGroup>
@@ -73,29 +75,33 @@ const ActivityScreen = () => {
         <ActivityEditWhiteboard></ActivityEditWhiteboard>
         <ActivityAddFriends></ActivityAddFriends>
         <ActivityDeleteFriends></ActivityDeleteFriends> */}
+        <ActivityExpense></ActivityExpense>
+        <ActivityPayment></ActivityPayment>
     </View>
   );
 };
 
-const notifycationMapping = {
-  addGroup: ActivityAddGroup,
-  deleteGroup: ActivityDeleteGroup,
-  editGroupName: ActivityEditGroupName,
-  editGroupAvatar: ActivityEditGroupAvatar,
-  addMember: ActivityAddMember,
-  deleteMember: ActivityDeleteMember,
-  editWhiteboard: ActivityEditWhiteboard,
-  addFriend: ActivityAddFriends,
-};
+// const notifycationMapping = {
+//   addGroup: ActivityAddGroup,
+//   deleteGroup: ActivityDeleteGroup,
+//   editGroupName: ActivityEditGroupName,
+//   editGroupAvatar: ActivityEditGroupAvatar,
+//   addMember: ActivityAddMember,
+//   deleteMember: ActivityDeleteMember,
+//   editWhiteboard: ActivityEditWhiteboard,
+//   addFriend: ActivityAddFriends,
+// };
 
-const NotificationRender = ({ type, data }) => {
-  const Component = notifycationMapping[type];
+// const NotificationRender = ({ type, data }) => {
+//   const Component = notifycationMapping[type];
 
-  if (!Component) {
-    return null;
-  }
+//   if (!Component) {
+//     return null;
+//   }
 
-  return <Component data={data} />;
-};
+//   return <Component data={data} />;
+// };
 
 export default ActivityScreen;
+
+
